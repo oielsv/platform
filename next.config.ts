db12183207c76f9d007ad5b652888c859baa23a5
oyelsv <oyelsv@gmail.cp>
2025-07-31 16:23:37 +0200

    init project

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  poweredByHeader: false,
+  reactStrictMode: true,
+};
+
+export default nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  safelist: ['dark'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
