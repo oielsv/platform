d059fabd0888d5d676a88f39cc874b24c4bd4012
oyelsv <oyelsv@gmail.cp>
2025-08-03 17:13:08 +0200

    feat(not-found): add local & global not found pages. ISSUE-6

---
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import { useTranslations } from 'next-intl';
+
+import { Button } from '@/components/ui/button';
+
+export function NotFoundPage() {
+  const t = useTranslations('notFound');
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 text-center">
+      <div className="w-[280px] h-[220px]">
+        <Image src="/images/404.png" alt="404" width={922} height={721} priority />
+      </div>
+      <span>{t('title')}</span>
+      <Button className="uppercase">{t('btn')}</Button>
+    </div>
+  );
+}
